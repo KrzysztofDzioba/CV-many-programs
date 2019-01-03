ec00925d22f8f2d11c2392c09565c01dbaaedb18
Krzysztof Dzioba - work ubuntu <krzysztof.dzioba.93@gmail.com>
2019-01-03 16:36:51 +0100

    Refactor DefaultPalindromeService

--- src/main/java/palindrome/implementation/DefaultPalindromeService.java
+++ src/main/java/palindrome/implementation/DefaultPalindromeService.java
@@ -6,17 +6,34 @@ public class DefaultPalindromeService implements PalindromeService {
 
     @Override
     public boolean canBePalindrome(String text) {
-        if (text.length() == 0 || text.length() == 1)
+        if (text.length() < 2)
             return true;
 
-        for (int i = 0; i < text.length() - 1; i++) {
-            if ((text.charAt(i)) == (text.charAt(text.length() - 1)))
-                return canBePalindrome(text.substring(i + 1, text.length() - 1));
-            for (int j = text.length(); j > i + 1; j--) {
-                if ((text.charAt(i)) == (text.charAt(j - 1)))
-                    return canBePalindrome(text.substring(i, j));
+        for (int leftCharIndex = 0; leftCharIndex < text.length() - 1; leftCharIndex++) {
+            if (charAtGivenIndexEqualToLastChar(text, leftCharIndex))
+                return canBePalindrome(trimFromGivenIndexToLastChar(text, leftCharIndex));
+            for (int rightCharIndex = text.length(); rightCharIndex > leftCharIndex + 1; rightCharIndex--) {
+                if (firstAndLastCharAreEqual(text, leftCharIndex, rightCharIndex))
+                    return canBePalindrome(trimStringFromIndexToIndex(text, leftCharIndex, rightCharIndex));
             }
         }
         return false;
+    }
+
+    private String trimStringFromIndexToIndex(String text, int leftIndex, int rightIndex) {
+        return text.substring(leftIndex, rightIndex);
+    }
+
+    private boolean firstAndLastCharAreEqual(String text, int leftIndex, int rightIndex) {
+        return text.charAt(leftIndex) == text.charAt(rightIndex - 1);
+    }
+
+    private String trimFromGivenIndexToLastChar(String text, int index) {
+        return text.substring(index + 1, text.length() - 1);
+    }
+
+    private boolean charAtGivenIndexEqualToLastChar(String text, int leftCharIndex) {
+        int lastCharIndex = text.length() - 1;
+        return text.charAt(leftCharIndex) == text.charAt(lastCharIndex);
     }
 }
